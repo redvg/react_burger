032adf61f8f3be897fd81d8ed2eb6c59bcade980
VG <master@philyn.com>
2018-07-20 23:06:22 +0200

    adds inline style

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Person.css';
 
+const style = {
+    border: '1px solid',
+    backgroundColor: 'yellow'
+}
+
 const person = (props) => 
     <div className='Person'> 
         <p>I am {props.name} person who is {props.age} years old</p>
         <p onClick={props.clickHandler}>{props.children}</p>
-        <input type="text" onChange={props.typedTextHandler} value={props.name}/>
+        <input type="text" onChange={props.typedTextHandler} value={props.name} style={style}/>
     </div>    
 
 export default person;
