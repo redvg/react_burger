8037f1bdf067b82860967fa698dd4daab5855878
VG <master@philyn.com>
2018-07-20 18:49:36 +0200

    manipulates state

--- src/App.js
+++ src/App.js
@@ -14,13 +14,22 @@ class App extends Component {
       {name: 'b', age: 2},
 
       {name: 'c', age: 3}
-    ]
+    ],
+
+    counter: 0
+  }
+
+  buttonClickHandler = () => {
+
+    this.setState({counter: this.state.counter + 1});
   }
 
   render() {
     return (
       <div className="App">
         <h1>some react app</h1>
+        <button onClick={this.buttonClickHandler}>click</button>
+        <p>{this.state.counter}</p>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > <p>cool!</p> </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
