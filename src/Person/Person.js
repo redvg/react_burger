0f1c454fa92cf68f2c83354bdcad184aac6a8394
VG <master@philyn.com>
2018-07-20 22:50:36 +0200

    removes dirty style

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => 
     <div> 
         <p>I am {props.name} person who is {props.age} years old</p>
-        <p onClick={props.clickHandler} style={{border: 'solid red 1px'}}>{props.children}</p>
+        <p onClick={props.clickHandler}>{props.children}</p>
         <input type="text" onChange={props.typedTextHandler} value={props.name}/>
     </div>    
 
